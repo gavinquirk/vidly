276b8329376adbf599b1da2bf6dd9b5931b4a0df
Gavin Quirk <gavinquirk@gmail.com>
2019-06-19 16:56:21 -0700

    edit customer action logic

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -37,9 +37,21 @@ namespace Vidly.Controllers
 
         // Create Customer
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
+            if (customer.Id == 0)
             _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+            }
+
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
