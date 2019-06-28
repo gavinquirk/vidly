a4cc0e8789b172f8cb50bd6005239b6a92a6d29e
Gavin Quirk <gavinquirk@gmail.com>
2019-06-28 12:49:41 -0700

    validation for movie api, added NumberAvailable to Movie

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -25,6 +25,10 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
+            // Check for movie ids
+            if (newRental.MovieIds.Count == 0)
+                return BadRequest("No Movie Ids have been given");
+
             // Get customer from context
             var customer = _context.Customers.SingleOrDefault(c => c.Id == newRental.CustomerId);
 
@@ -32,10 +36,18 @@ namespace Vidly.Controllers.Api
                 return BadRequest("Invalid Customer ID");
 
             // Get movies from context
-            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            if (movies.Count != newRental.MovieIds.Count)
+                return BadRequest("One or more MovieIds are invalid");
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available");
+
+                movie.NumberAvailable--;
+
                 var rental = new Rental
                 {
                     Customer = customer,
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -12,16 +12,23 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         public Genre Genre { get; set; }
+
         [Display(Name = "Genre")]
         [Required]
         public byte GenreId { get; set; }
+
         [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         public DateTime DateAdded { get; set; }
+
         [Display(Name = "Number in Stock")]
         [Range(1, 100)]
         public int Stock { get; set; }
+
+        public byte NumberAvailable { get; set; }
     }
 
 }
